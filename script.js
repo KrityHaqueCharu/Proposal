32b0dc618c04d8eaa2c300caa63fcb3de251f492
KrityHaqueCharu <krity.haque@gmail.com>
2021-04-02 19:12:26 +0600

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yes").addEventListener("click",f);
+function f(){
+    document.getElementById("h").innerHTML = "YAAY!!!";
+    //console.log("hel");
+    document.getElementById("n").innerText="I love You most!";
+    //console.log("hoy");
+}
